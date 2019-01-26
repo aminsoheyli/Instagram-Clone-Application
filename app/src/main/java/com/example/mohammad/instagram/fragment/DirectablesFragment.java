64cf9332e4c5555090e444ba9e6eec155f86e2bd
safari <>
2019-01-25 23:04:32 -0500

    Fixed main queries

--- app/src/main/java/com/example/mohammad/instagram/fragment/DirectablesFragment.java
+++ app/src/main/java/com/example/mohammad/instagram/fragment/DirectablesFragment.java
@@ -127,18 +127,14 @@ public class DirectablesFragment extends Fragment {
     private ArrayList<ProfileCard> homeFragmentInformationsQuery() {
         ArrayList<ProfileCard> informations = new ArrayList<>();
         // Home fragment informations query
-        return informations;
-    }
-
-    private ArrayList<ProfileCard> globalFragmentInformationsQuery() {
-        //Query --> posts.add(Post)
-//        Cursor c = MainActivity.db.rawQuery("select * from post order by post_date desc;", null);
-        ArrayList<ProfileCard> informations = new ArrayList<>();
-        Cursor c = MainActivity.db.rawQuery("select * from post where user_id !='" + MainActivity.currentUserId + "' order by post_date desc;", null);
-        if (c.moveToFirst()) {
+ArrayList<ProfileCard> informations = new ArrayList<>();
+        Cursor c = MainActivity.db.rawQuery("select distinct * from post, follow where post.user_id !='" + MainActivity.currentUserId + "' and follow.follower_id = '"+ MainActivity.currentUserId +"' order by post.post_date desc;", null);
+        
+            if (c.moveToFirst()) {
+				do{
             Cursor cc = MainActivity.db.rawQuery("select count(user_id) from likes;", null);
-            Cursor ccc = MainActivity.db.rawQuery("select count(user_id) from likes where user_id = '" + MainActivity.currentUserId + "' order by post_date desc;", null);
-            Cursor cccc = MainActivity.db.rawQuery("select count(user_id) from save where user_id = '" + MainActivity.currentUserId + "' order by post_date desc;", null);
+            Cursor ccc = MainActivity.db.rawQuery("select count(user_id) from likes where user_id = '" + MainActivity.currentUserId + "' and post_id = '"+ c.getString(0) +"';", null);
+            Cursor cccc = MainActivity.db.rawQuery("select count(user_id) from save where user_id = '" + MainActivity.currentUserId + "'  and post_id = '"+ c.getString(0) +"';", null);
             boolean liked = false;
             boolean saved = false;
             if (ccc.moveToFirst()) {
@@ -159,32 +155,46 @@ public class DirectablesFragment extends Fragment {
                         );
                 informations.add(temp);
 
+            }while(c.moveToNext());
+                cc.close();
             }
-            while (c.moveToNext()) {
-                cc = MainActivity.db.rawQuery("select count(user_id) from post order by post_date desc;", null);
-//                    ccc = MainActivity.db.rawQuery("select count(user_id) from likes where user_id = '" + MainActivity.currentUserId + "' order by post_date desc;", null);
-//                    cccc = MainActivity.db.rawQuery("select count(user_id) from save where user_id = '" + MainActivity.currentUserId + "' order by post_date desc;", null);
-                liked = false;
-                saved = false;
-//                    if (ccc.moveToFirst()) {
-//                        liked = !ccc.getString(0).matches("0");
-//                    }
-//                    if (cccc.moveToFirst()) {
-//                        saved = !cccc.getString(0).matches("0");
-//                    }
-                if (cc.moveToFirst()) {
-                    ProfileCard temp =
-                            new ProfileCard(c.getString(0),
-                                    BitmapFactory.decodeByteArray(c.getBlob(3), 0, c.getBlob(3).length),
-                                    c.getString(1),
-                                    cc.getString(0),
-                                    c.getString(4),
-                                    c.getString(2),
-                                    liked,
-                                    saved
-                            );
-                    informations.add(temp);
-                }
+        }
+        c.close();
+        return informations;
+    }
+
+    private ArrayList<ProfileCard> globalFragmentInformationsQuery() {
+        //Query --> posts.add(Post)
+//        Cursor c = MainActivity.db.rawQuery("select * from post order by post_date desc;", null);
+        ArrayList<ProfileCard> informations = new ArrayList<>();
+        Cursor c = MainActivity.db.rawQuery("select * from post where user_id !='" + MainActivity.currentUserId + "' order by post_date desc;", null);
+        
+            if (c.moveToFirst()) {
+				do{
+            Cursor cc = MainActivity.db.rawQuery("select count(user_id) from likes;", null);
+            Cursor ccc = MainActivity.db.rawQuery("select count(user_id) from likes where user_id = '" + MainActivity.currentUserId + "' and post_id = '"+ c.getString(0) +"';", null);
+            Cursor cccc = MainActivity.db.rawQuery("select count(user_id) from save where user_id = '" + MainActivity.currentUserId + "'  and post_id = '"+ c.getString(0) +"';", null);
+            boolean liked = false;
+            boolean saved = false;
+            if (ccc.moveToFirst()) {
+                liked = (ccc.getString(0).matches("0")) ? false : true;
+            }
+            if (cccc.moveToFirst()) {
+                saved = (cccc.getString(0).matches("0")) ? false : true;
+            }
+            if (cc.moveToFirst()) {
+                ProfileCard temp =
+                        new ProfileCard(c.getString(0), BitmapFactory.decodeByteArray(c.getBlob(3), 0, c.getBlob(3).length),
+                                c.getString(1),
+                                cc.getString(0),
+                                c.getString(4),
+                                c.getString(2),
+                                liked,
+                                saved
+                        );
+                informations.add(temp);
+
+            }while(c.moveToNext());
                 cc.close();
             }
         }
@@ -195,7 +205,43 @@ public class DirectablesFragment extends Fragment {
     private ArrayList<ProfileCard> savedFragmentInformationsQuery() {
         ArrayList<ProfileCard> informations = new ArrayList<>();
         // Saved fragment informations query
+//Query --> posts.add(Post)
+//        Cursor c = MainActivity.db.rawQuery("select * from post order by post_date desc;", null);
+        ArrayList<ProfileCard> informations = new ArrayList<>();
+        Cursor c = MainActivity.db.rawQuery("select * from post, save where post.user_id !='" + MainActivity.currentUserId + "' and save.post_id = post.post_id order by post.post_date desc;", null);
+        
+            if (c.moveToFirst()) {
+				do{
+            Cursor cc = MainActivity.db.rawQuery("select count(user_id) from likes;", null);
+            Cursor ccc = MainActivity.db.rawQuery("select count(user_id) from likes where user_id = '" + MainActivity.currentUserId + "' and post_id = '"+ c.getString(0) +"';", null);
+            Cursor cccc = MainActivity.db.rawQuery("select count(user_id) from save where user_id = '" + MainActivity.currentUserId + "'  and post_id = '"+ c.getString(0) +"';", null);
+            boolean liked = false;
+            boolean saved = false;
+            if (ccc.moveToFirst()) {
+                liked = (ccc.getString(0).matches("0")) ? false : true;
+            }
+            if (cccc.moveToFirst()) {
+                saved = (cccc.getString(0).matches("0")) ? false : true;
+            }
+            if (cc.moveToFirst()) {
+                ProfileCard temp =
+                        new ProfileCard(c.getString(0), BitmapFactory.decodeByteArray(c.getBlob(3), 0, c.getBlob(3).length),
+                                c.getString(1),
+                                cc.getString(0),
+                                c.getString(4),
+                                c.getString(2),
+                                liked,
+                                saved
+                        );
+                informations.add(temp);
+
+            }while(c.moveToNext());
+                cc.close();
+            }
+        }
+        c.close();
         return informations;
+
     }
 
 
